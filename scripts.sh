17fc432bbc3fdb340e5fa2cfc2545a9dc5a10ada
zippiy <3387957+zippiy@users.noreply.github.com>
2020-05-16 17:25:02 +0200

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -5,3 +5,6 @@ snowsql -a $account -u $user -d $db -s public
 2. 
 list @~/staged
 COPY INTO Products FROM '@~/staged/' file_format = (format_name = csv1)   pattern='.*txt.gz'
+
+3.
+select count(*), seller from "ALLEGRO"."PUBLIC"."PRODUCTS" group by seller HAVING COUNT(*) > 10;
