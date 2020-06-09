4a07560c58d07cfc8b9a33c953c73f6cb71a2dcf
zippiy <3387957+zippiy@users.noreply.github.com>
2020-06-09 17:06:51 +0200

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -7,6 +7,8 @@ snowsql -a %account% -u %user% -d %db% -s public
 
 2. 
 put file://C:\temp\set*.txt @~/staged/set;
+put file://./c*.txt @~/staged/set;
+
 list @~/staged;
 COPY INTO Products FROM '@~/staged/' file_format = (format_name = csv1)   pattern='.*txt.gz'
 COPY INTO Seller2Keyword FROM '@~/staged/set' file_format = (format_name = csv1)   pattern='.*txt.gz'
